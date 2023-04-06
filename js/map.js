d2612dc425955cc5f795572cd1fc097ee8ce2481
Siestaa <lex.qz97@gmail.com>
2023-04-06 22:03:57 +0500

    fix src pin

--- js/map.js
+++ js/map.js
@@ -4,13 +4,13 @@ const map = L.map('map-canvas');
 const markerGroup = L.layerGroup().addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: '/img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52]
 });
 
 const adPinIcon = L.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: '/img/pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52]
 });
